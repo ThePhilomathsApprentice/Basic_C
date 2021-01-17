23b47794208dd2a2a5882768c16e10f25afa5e06
Yash Joshi <yashjoshi3995@gmail.com>
2021-01-17 19:16:10 +0530

    17_01_2021: Major Update, Added an exmple of using libraries.

---
+++ Libraries/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "myHeader.h"
+
+int  main(){
+    
+    int a=20;
+    int b=10;
+    
+    printf("The sum of %d and %d is %d\n",a,b,sum(a,b));
+    
+    printf("The difference of %d and %d is %d\n",a,b,difference(a,b));
+    
+    
+    
+    
+    
+    
+    }
